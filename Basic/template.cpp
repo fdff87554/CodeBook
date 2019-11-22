ab6c9fed6c216e736c737bb6b3c6070197c34ef0
CrazyFire <fdff87554@gmail.com>
2019-11-22 20:51:56 +0800

    add template

---
+++ Basic/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0)
+
+//structure
+
+//declaration
+
+//functions
+
+int main(void){
+	IOS;
+
+
+	return 0;
+}
